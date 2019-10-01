0a923682cda7f2f1be6c31846de03fb82cf21ee1
yachen <yachen.tw@gmail.com>
2019-10-01 20:08:18 +0800

    20191001-cloud_dl

---
+++ tmp/httpserv.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+import socket
+
+PORT = 8080
+class showipHTTPHandler(http.server.BaseHTTPRequestHandler):
+    def do_HEAD(self, s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+    def do_GET(self, s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        retstr = "<html><head><title>hello</title></head><body> hello %s </body></html>" \
+        % socket.gethostbyname(socket.gethostname())
+        s.wfile.write(retstr.encode())
+httpd  = socketserver.TCPServer(("", PORT), showipHTTPHandler)
+print("serving at port", PORT)
+httpd.serve_forever()
